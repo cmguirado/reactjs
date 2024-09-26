24886158c8e55564bd97a47f62b8e528c231f4aa
Carlos Guirado <ingcarlosguirado@gmail.com>
2024-09-26 10:28:09 -0300

    primer commit

---
+++ components/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <h2>NavBar</h2>
+        </nav>
+    )
+}
+
+//export default NavBar
+export {NavBar}
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import NavBar from '../components/NavBar'
+import { NavBar } from '../components/NavBar'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hola</h1>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
